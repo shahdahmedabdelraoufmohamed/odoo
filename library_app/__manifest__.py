88dd4e0d17868e621dc07cd5c32bb8f7270d1eda
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-03-11 07:57:00 +0200

    task1

---
+++ library_app/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Library App",
+    'summary': """ """,
+    'description': """ """,
+    'author': "Shahd Ahmed",
+    'category': 'Productivity',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+     'security/ir.model.access.csv',
+      'views/base_menus.xml',
+      'views/book.xml',
+
+    ],
+}
+
---
+++ todo_app/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Todo App",
+    'summary': """ """,
+    'description': """ """,
+    'author': "Shahd Ahmed",
+    'category': 'Productivity',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+     'security/ir.model.access.csv',
+      'views/base_menus.xml',
+      'views/ticket.xml',
+
+    ],
+}
